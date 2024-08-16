220102fb4499b2761e069479c3476d755e90e9bf
Stern. <90482851+SternI@users.noreply.github.com>
2024-08-16 14:56:46 +0300

    31.00 LastSubmitTime, LastRenderTime

--- Offsets.h
+++ Offsets.h
@@ -5,4 +5,6 @@ namespace Offsets {
 
     uintptr_t Mesh = 0x310; // ACharacter->Mesh
     uintptr_t BoneArray = 0x570;
+    uintptr_t LastSubmitTime = 0x328;
+    uintptr_t LastRenderTime = 0x330;
 }
