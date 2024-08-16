de8dde8804d20a151e7859da49ec6aa896d576fe
Stern. <90482851+SternI@users.noreply.github.com>
2024-08-16 14:49:09 +0300

    31.00 Mesh, BoneArray

--- Offsets.h
+++ Offsets.h
@@ -2,4 +2,7 @@ namespace Offsets {
     uintptr_t UWorld = 0x11ECD148;
     uintptr_t GObjects = 0x11EA4EE0;
     uintptr_t GNames = 0x120A7A40;
+
+    uintptr_t Mesh = 0x310; // ACharacter->Mesh
+    uintptr_t BoneArray = 0x570;
 }
