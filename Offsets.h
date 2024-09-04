117b48fc8299b5f5ff38abbfb817d27bcb9fd6e3
Stern. <90482851+SternI@users.noreply.github.com>
2024-09-04 15:43:18 +0300

    31.10 va_text, UWorld

--- Offsets.h
+++ Offsets.h
@@ -1,6 +1,6 @@
 namespace Offsets {
-    uintptr_t va_text = 0x602BC0BB;
-    uintptr_t UWorld = 0x11FCBD78;
+    uintptr_t va_text = 0;
+    uintptr_t UWorld = 0x11FD4D78;
     uintptr_t GObjects = 0x11EA4EE0;
     uintptr_t GNames = 0x120A7A40;
 
