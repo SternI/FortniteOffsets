10c38309a1266b94e60a857d873994e7015085f0
Stern. <90482851+SternI@users.noreply.github.com>
2024-09-04 20:42:06 +0300

    Update va_text.h

--- va_text.h
+++ va_text.h
@@ -1,5 +1,5 @@
-for (auto i = 0; i < 16; i++) {
-    va_text = virtualaddy + i * 0x1000;
+for (auto i = 0; i < INT_MAX; i++) {
+    va_text = BaseAddress + i * 0x1000;
     auto uworld = read<uintptr_t>(va_text + Offsets::UWorld);
     auto level = read<uintptr_t>(uworld + Offsets::PersistentLevel);
     if (uworld && level && read<uintptr_t>(level + Offsets::OwningWorld) == uworld) {
