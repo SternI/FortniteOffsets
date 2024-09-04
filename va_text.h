543847e9ee4fd99b211d4297661df7920043c417
Stern. <90482851+SternI@users.noreply.github.com>
2024-09-04 14:41:20 +0300

    31.10 va_text

--- va_text.h
+++ va_text.h
@@ -1,6 +1,6 @@
 // People will steal the updated one without credit :(
 for (auto i = 0; i < 25; i++) {
-    if (Memory::Read<__int32>(BaseAddress + (i * 0x1000) + 0x250) == 0x6F41C00) {
+    if (Memory::Read<__int32>(BaseAddress + (i * 0x1000) + 0x250) == 0x602BC0BB) {
         va_text = BaseAddress + ((i + 1) * 0x1000);
     }
 }
