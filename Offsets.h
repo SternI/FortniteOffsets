935a0ceda07301c3414c44c30f7be592d3f3adbe
Stern. <90482851+SternI@users.noreply.github.com>
2024-09-11 04:19:09 +0300

    31.10 GObjects, GNames

--- Offsets.h
+++ Offsets.h
@@ -1,7 +1,7 @@
 namespace Offsets {
     uintptr_t UWorld = 0x11FD3D78;
-    uintptr_t GObjects = 0x11EA4EE0;
-    uintptr_t GNames = 0x120A7A40;
+    uintptr_t GObjects = 0x11FABA60;
+    uintptr_t GNames = 0x121B1400;
 
     uintptr_t Mesh = 0x310; // ACharacter->Mesh
     uintptr_t BoneArray = 0x570;
