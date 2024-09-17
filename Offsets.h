a70490d90780fb7a8d742f13013ebd9d31fad83c
Stern. <90482851+SternI@users.noreply.github.com>
2024-09-17 17:30:18 +0300

    31.20 Offsets

--- Offsets.h
+++ Offsets.h
@@ -1,14 +1,14 @@
 namespace Offsets {
-    uintptr_t UWorld = 0x11FD3D78;
-    uintptr_t GObjects = 0x11FABA60;
-    uintptr_t GNames = 0x121B1400;
+    uintptr_t UWorld = 0x1215C178;
+    uintptr_t GObjects = 0x12133D70;
+    uintptr_t GNames = 0x1233C1A0;
 
     uintptr_t Mesh = 0x310; // ACharacter->Mesh
     uintptr_t BoneArray = 0x570;
     uintptr_t LastSubmitTime = 0x2E0;
     uintptr_t LastRenderTime = 0x2E8;
-    uintptr_t TeamIndex = 0x1201;
-    uintptr_t ProjectileSpeed = 0x1D20;
-    uintptr_t ProjectileGravity = 0x1D24;
-    uintptr_t PlayerName = 0xA98;
+    uintptr_t TeamIndex = 0x1211;
+    uintptr_t ProjectileSpeed = 0x1E40;
+    uintptr_t ProjectileGravity = 0x1E44;
+    uintptr_t PlayerName = 0xAA8;
 }
